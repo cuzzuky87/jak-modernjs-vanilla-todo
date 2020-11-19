24ab2c21645a2b08e635d972c287a9beda1375f4
cuzzuky87 <39122919+cuzzuky87@users.noreply.github.com>
2020-11-19 23:59:02 +0900

    文字列入力

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert("pushed");
+  //テキストボックスの値を取得し、クリアを行う。
+  const inputText = document.getElementById("add-task-text").value;
+  document.getElementById("add-task-text").value = "";
+
+  if (inputText === "") {
+    return;
+  }
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+  console.log(inputText);
 };
 
 document
