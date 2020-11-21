610e214c7cc88db8ea21a3880d7dd97440587afe
cuzzuky87 <39122919+cuzzuky87@users.noreply.github.com>
2020-11-21 14:21:57 +0900

    戻す実装

--- src/index.js
+++ src/index.js
@@ -9,19 +9,60 @@ const onClickAdd = () => {
     return;
   }
 
+  createIncompleteTask(inputText);
+};
+
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-task-list").removeChild(target);
+};
+
+//未完了リストに追加する関数
+const createIncompleteTask = (taskText) => {
   //div生成
   const div = document.createElement("div");
   div.id = "list-row";
 
   //liタグ生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = taskText;
 
   //button(完了)タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("done");
+    //対象取得
+    const doneTarget = completeButton.parentNode;
+
+    //未完了から削除
+    deleteFromIncompleteList(doneTarget);
+    //todo内容テキスト取得
+    const text = doneTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    doneTarget.textContent = null;
+
+    //liタグ生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //戻すボタン
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const backTarget = backButton.parentNode;
+      document.getElementById("done-task-list").removeChild(backTarget);
+
+      //タスク内容取得
+      const text = backTarget.firstElementChild.innerText;
+      createIncompleteTask(text);
+    });
+
+    //タグ生成
+    doneTarget.appendChild(li);
+    doneTarget.appendChild(backButton);
+
+    //完了に追加
+    document.getElementById("done-task-list").appendChild(doneTarget);
   });
 
   //button(削除)タグ生成
@@ -29,7 +70,7 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-task-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteTarget);
   });
 
   // divタグの子要素に各要素を設定
